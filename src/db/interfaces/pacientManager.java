caccc5611a1f3a8a26087776bc598daaf6b0e3c6
diego <diego@LAPTOP-2ET5KB0J>
2020-03-19 19:09:05 +0100

    .

--- src/db/interfaces/pacientManager.java
+++ src/db/interfaces/pacientManager.java
@@ -1,9 +1,16 @@
 package db.interfaces;
 
+import java.util.List;
 import pojos.*;
+
 
 public interface pacientManager {
 
 	public void add (Pacient pacient);
+	// public void releaseDate
+	public void update (Pacient pacient);
+	public List<Pacient> searchByName (Pacient pacient);
+	public List<Pacient> searchById (Pacient pacient);
+	// checkActive
 	
 }
