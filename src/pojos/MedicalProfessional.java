8f2fd168da8a024de82f967bc66b5ddae4854c6e
marta <marta@LAPTOP-V95ANNT4.home>
2020-03-19 18:38:58 +0100

    atributos medical professionals

--- src/pojos/MedicalProfessional.java
+++ src/pojos/MedicalProfessional.java
@@ -1,5 +1,97 @@
 package pojos;
 
-public class MedicalProfessional {
+import java.io.Serializable;
+import java.sql.Blob;
+import java.sql.Date;
 
+public class MedicalProfessional implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -6497160293984561608L;
+	private Integer id;
+	private String name;
+	private Date dob;
+	private Sex sex;
+	private String profession;
+	private Blob photo;
+	public enum Sex {MALE, FEMALE}
+	
+	
+	
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Date getDob() {
+		return dob;
+	}
+	public void setDob(Date dob) {
+		this.dob = dob;
+	}
+	public Sex getSex() {
+		return sex;
+	}
+	public void setSex(Sex sex) {
+		this.sex = sex;
+	}
+	public String getProfession() {
+		return profession;
+	}
+	public void setProfession(String profession) {
+		this.profession = profession;
+	}
+	public Blob getPhoto() {
+		return photo;
+	}
+	public void setPhoto(Blob photo) {
+		this.photo = photo;
+	}
+	
+	public MedicalProfessional() {
+		super();
+	}
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		MedicalProfessional other = (MedicalProfessional) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+	
+	@Override
+	public String toString() {
+		return "MedicalProfessional [id=" + id + ", name=" + name + ", dob=" + dob + ", sex=" + sex + ", profession="
+				+ profession + "]";
+	}
+	
+	
 }
+
