2755ffb574bfa9eff586dda30aa1c4cfda622110
Ignacio <Ignacio@192.168.1.59>
2020-03-19 18:35:33 +0100

    Department

--- src/pojos/Department.java
+++ src/pojos/Department.java
@@ -1,5 +1,91 @@
 package pojos;
 
-public class Department {
+import java.io.Serializable;
 
+public class Department implements Serializable {
+
+	
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -6234791722618349000L;
+	private Integer id;
+	private String name;
+	private int floor;
+	private float budget;
+	private Integer boss_id;
+	
+	@Override
+	public String toString() {
+		return "Department [id=" + id + ", name=" + name + ", floor=" + floor + ", budget=" + budget + ", boss_id="
+				+ boss_id + "]";
+	}
+	
+	
+	
+
+	public Department() {
+		super();
+	}
+
+
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Department other = (Department) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+	
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getFloor() {
+		return floor;
+	}
+	public void setFloor(int floor) {
+		this.floor = floor;
+	}
+	public float getBudget() {
+		return budget;
+	}
+	public void setBudget(float budget) {
+		this.budget = budget;
+	}
+	public Integer getBoss_id() {
+		return boss_id;
+	}
+	public void setBoss_id(Integer boss_id) {
+		this.boss_id = boss_id;
+	}
+	
+	
 }
