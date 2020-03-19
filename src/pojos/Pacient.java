fb42523dd6e99843d005779d5b5a357d04777247
diego <diego@LAPTOP-2ET5KB0J>
2020-03-19 18:06:25 +0100

    atributos clase pacient

--- src/pojos/Pacient.java
+++ src/pojos/Pacient.java
@@ -1,5 +1,14 @@
 package pojos;
 
+import java.sql.Date;
+
 public class Pacient {
 
+	private Integer id;
+	private String name;
+	private String sex;
+	private Date dob;
+	private Boolean intern;
+	private String nie;
+	
 }
