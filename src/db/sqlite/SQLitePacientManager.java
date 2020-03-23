5cd2fbc109d147e0fea2aafbba36d7ed9569ba6b
diego <diego@LAPTOP-2ET5KB0J>
2020-03-23 18:17:36 +0100

    .

---
+++ src/db/sqlite/SQLitePacientManager.java
@@ -0,0 +1,34 @@
+package db.sqlite;
+
+import java.util.List;
+
+import db.interfaces.PacientManager;
+import pojos.Pacient;
+
+public class SQLitePacientManager implements PacientManager {
+
+	@Override
+	public void add(Pacient pacient) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void update(Pacient pacient) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public List<Pacient> searchByName(String name) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Pacient> searchById(Integer id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
