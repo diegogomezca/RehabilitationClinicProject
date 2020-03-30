390f161a147e705179a04e13fc17dbb9c0202d3c
diego <diego@LAPTOP-2ET5KB0J>
2020-03-30 16:51:25 +0200

    .

--- src/db/interfaces/PacientManager.java
+++ src/db/interfaces/PacientManager.java
@@ -12,19 +12,7 @@ public interface PacientManager {
 	public List<Pacient> searchByName (String name);
 	public Pacient searchById (Integer id);
 	public void deleteById(Integer id);
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
->>>>>>> branch 'master' of https://github.com/diegogomezca/RehabilitationClinicProject.git
 	public void updatePacient(Pacient pacient);
 
-<<<<<<< HEAD
-=======
->>>>>>> branch 'master' of https://github.com/diegogomezca/RehabilitationClinicProject
->>>>>>> branch 'master' of https://github.com/diegogomezca/RehabilitationClinicProject
-	// checkActive
->>>>>>> branch 'master' of https://github.com/diegogomezca/RehabilitationClinicProject.git
-	
+
 }
